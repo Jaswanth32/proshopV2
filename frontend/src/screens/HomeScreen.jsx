d5a08ea2483ef8e5102e59e5eec59f43817bddad
Jaswanth32 <jalla.jaswanth2.com>
2023-09-23 14:21:05 +0530

    redux toolkit added

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -2,16 +2,21 @@ import { Row, Col } from "react-bootstrap";
 import Product from "../components/Product";
 import axios from "axios";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
-
+import Loader from "../components/Loader";
+import Message from "../components/Message";
 const HomeScreen = () => {
   const { data: products, isLoading, error } = useGetProductsQuery();
 
   return (
     <>
       {isLoading ? (
-        <h1>Loading.....</h1>
+        <Loader />
       ) : error ? (
-        <div>{error?.data?.message || error.error}</div>
+        <div>
+          <Message variant="danger">
+            {error?.data?.message || error.error}
+          </Message>
+        </div>
       ) : (
         <>
           <h1>Latest Products</h1>
